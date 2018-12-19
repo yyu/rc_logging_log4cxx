b7c26194ba8314a622f57f2413cf352156d4fbde
yy <forresty@amazon.com>
2018-12-19 00:47:08 +0000

    it builds

--- rcl_logging_log4cxx/src/rcl_logging_log4cxx/rcl_logging_log4cxx.cpp
+++ rcl_logging_log4cxx/src/rcl_logging_log4cxx/rcl_logging_log4cxx.cpp
@@ -114,7 +114,7 @@ rcl_logging_ret_t rcl_logging_external_initialize(const char * config_file)
     log4cxx::LayoutPtr layout(new log4cxx::PatternLayout(LOG4CXX_STR("%m%n")));
     char log_name_buffer[64];
     snprintf(log_name_buffer, sizeof(log_name_buffer), DEFAULT_LOG_FILE, GET_PID());
-    log4cxx::FileAppenderPtr file_appender(new log4cxx::FileAppender(layout, LOG4CXX_STR(log_name_buffer),
+    log4cxx::FileAppenderPtr file_appender(new log4cxx::FileAppender(layout, LOG4CXX_STR("foo"),
       true));
     root_logger->addAppender(file_appender);
   }
